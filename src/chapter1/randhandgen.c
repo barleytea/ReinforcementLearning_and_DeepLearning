50532b2437e48943b056c9f443bfde7edf85e661
barleytea <barleytea362@gmail.com>
2018-05-13 13:49:55 +0900

    adjust comment format

--- src/chapter1/randhandgen.c
+++ src/chapter1/randhandgen.c
@@ -1,8 +1,4 @@
-/*****************************/
-/*                           */
 /* generate 1000 biased hand */
-/*                           */
-/*****************************/
 
 #define _CRT_SECURE_NO_WARNINGS
 
